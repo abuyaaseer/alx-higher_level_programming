860092dc6a4a35dd47c326451b1f956f6e111ab6
Abdulkareem Bello <abdulkareembello93@gmail.com>
2022-12-01 23:42:10 +0100

    added colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 import sys
 n = len(sys.argv)
 if n == 2:
-    print("{} argument".format(n-1))
+    print("{} argument:".format(n-1))
 else:
-    print("{} arguments".format(n-1))
+    print("{} arguments:".format(n-1))
 for x in range(1, n):
     print("{}: {}".format(x, sys.argv[x]))
