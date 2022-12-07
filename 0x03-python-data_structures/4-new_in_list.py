de5895bd12435b1c067a0d4bb0f1b97afd41343e
Abdulkareem Bello <abdulkareembello93@gmail.com>
2022-12-07 23:30:21 +0100

    changed return value

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
 
     lenth = len(my_list)
 
     if idx > lenth - 1:
-        return None
+        return my_liist
 
     new_list = my_list[:]
     new_list[idx] = element
