14d14b131813de35d89da858ec9a568cf4edd69f
Abdulkareem Bello <abdulkareembello93@gmail.com>
2022-12-20 00:22:30 +0100

    typo correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a/b
-    except def ZeroDivisionError:
+        div = a / b
+    except ZeroDivisionError:
         div = None
     finally:
         print("Inside result: {}".format(div))
