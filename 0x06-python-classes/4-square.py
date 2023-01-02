42367c3a6ce9fe098df0d648feafbe0f7713806d
Abdulkareem Bello <abdulkareembello93@gmail.com>
2023-01-02 23:56:55 +0100

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ class Square that defines a square"""
 
+
 class Square:
     """ class Square that defines a square"""
-    def __init__(self, size=0)
+    def __init__(self, size=0):
         """ init square
+
         Args:
             value (int): size of the square.
         """
@@ -13,6 +15,7 @@ class Square:
     @property
     def size(self):
         """int: private size.
+
         Returns:
             Private size.
         """
@@ -21,6 +24,7 @@ class Square:
     @size.setter
     def size(self, value):
         """Sets value into size, must be int.
+
         Args:
             value (int): size of the square.
         """
@@ -29,11 +33,12 @@ class Square:
         elif value < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = value
+            self.__size = value  #: size of the square
 
     def area(self):
         """returns the area
+
         Returns:
             area.
         """
-        return self.__size ** 2
+        return self.__size**2
