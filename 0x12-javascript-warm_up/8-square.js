e8b06689532f9cde6a4ec53d80bc03595dfb0249
abuyaaseer <abdulkareembello93@gmail.com>
2023-03-14 18:22:07 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = Number(process.argv[2]);
+if (isNaN(args)) console.log('Missing size');
+else {
+  for (let i = 0; i < args; i++) {
+    let row = '';
+    for (let j = 0; j < args; j++) row += 'X';
+    console.log(row);
+  }
+}
