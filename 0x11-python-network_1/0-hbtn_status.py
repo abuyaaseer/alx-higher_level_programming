10876e19c398d01c2d92cb1938e78895675f6b9e
Abdulkareem <abdulkareembello93@gmail.com>
2023-04-03 10:54:06 +0100

    added urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as get:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as get:
         value = get.read()
         print("Body response:")
         print("\t- type: {}".format(type(value)))
